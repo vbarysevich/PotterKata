f6f06d9b1489c40dc4d988e730e8d34cc2cde242
vbarysevich <v.barysevich@godeltech.com>
2015-03-19 13:29:40 +0300

    tests for the implementation

---
+++ AO.KataPotter/AO.KataPotter.Implementation/Entities/Book.cs
@@ -0,0 +1,28 @@
+﻿using AO.KataPotter.Interfaces.Entities;
+
+namespace AO.KataPotter.Implementation.Entities
+{
+    public class Book : IBook
+    {
+        public Book()
+        {
+            
+        }
+        public Book(string name)
+        {
+            
+        }
+
+        public string Name
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
--- AO.KataPotter/AO.KataPotter.Tests/Entities/BookTestsSteps.cs
+++ AO.KataPotter/AO.KataPotter.Tests/Entities/BookTestsSteps.cs
@@ -1,26 +1,31 @@
-﻿using TechTalk.SpecFlow;
+﻿using AO.KataPotter.Implementation.Entities;
+using AO.KataPotter.Interfaces.Entities;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
 
 namespace AO.KataPotter.Tests.Entities
 {
     [Binding]
     public class BookTestsSteps
     {
+        private IBook _book;
+
         [Given(@"I create a new Book Instance with empty ctor")]
         public void GivenICreateANewBookInstanceWithEmptyCtor()
         {
-            ScenarioContext.Current.Pending();
+            _book = new Book();
         }
 
         [Given(@"I create a new Book Instance with name ""(.*)"" to ctor")]
         public void GivenICreateANewBookInstanceWithNameToCtor(string bookName)
         {
-            ScenarioContext.Current.Pending();
+            _book = new Book(bookName);
         }
 
         [When(@"Book Name property should have ""(.*)"" value")]
-        public void WhenBookNamePropertyShouldHaveValue(string bookName)
+        public void WhenBookNamePropertyShouldHaveValue(string expectedBookName)
         {
-            ScenarioContext.Current.Pending();
+            Assert.AreEqual(expectedBookName, _book.Name);
         }
     }
 }
