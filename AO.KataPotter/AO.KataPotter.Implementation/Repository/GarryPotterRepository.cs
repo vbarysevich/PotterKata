2c9dd4d90b3b2d43091a002ffa0e08952191dbc1
vbarysevich <v.barysevich@godeltech.com>
2015-03-19 13:51:44 +0300

    repository implementation

---
+++ AO.KataPotter/AO.KataPotter.Implementation/Repository/GarryPotterRepository.cs
@@ -0,0 +1,17 @@
+﻿using AO.KataPotter.Interfaces.Repository;
+
+namespace AO.KataPotter.Implementation.Repository
+{
+    public class GarryPotterRepository : IRepository
+    {
+        public Interfaces.Entities.IBook FindBookByName(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int BookCount
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+    }
+}
