325c09d0fd48e98fa484830f51af60f30db59087
vbarysevich <v.barysevich@godeltech.com>
2015-03-19 17:21:11 +0300

    Fix implementation for the latest test

--- AO.KataPotter/AO.KataPotter.Implementation/Entities/ShoppingCartItem.cs
+++ AO.KataPotter/AO.KataPotter.Implementation/Entities/ShoppingCartItem.cs
@@ -10,5 +10,24 @@ namespace AO.KataPotter.Implementation.Entities
         public int Quantity { get; set; }
 
         public IBook Book { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == this)
+            {
+                return true;
+            }
+            var item = obj as ShoppingCartItem;
+            if (item != null)
+            {
+                return this.Book.Name.Equals(item.Book.Name);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Book.Name.GetHashCode();
+        }
     }
 }
