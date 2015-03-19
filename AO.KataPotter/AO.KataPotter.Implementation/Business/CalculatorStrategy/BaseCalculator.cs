3b7d2598dc75d658bbfedb0f19445ff6533ede01
vbarysevich <v.barysevich@godeltech.com>
2015-03-19 15:38:34 +0300

    first calc implementation

--- AO.KataPotter/AO.KataPotter.Implementation/Business/CalculatorStrategy/BaseCalculator.cs
+++ AO.KataPotter/AO.KataPotter.Implementation/Business/CalculatorStrategy/BaseCalculator.cs
@@ -6,11 +6,13 @@ namespace AO.KataPotter.Implementation.Business.CalculatorStrategy
 {
     public abstract class BaseCalculator : ICalculator
     {
+        protected const decimal DEFAULT_PRICE = 8m;
+
         protected Dictionary<int, decimal> DistinctDiscounts = new Dictionary<int, decimal>
         {
             { 2, 5m }, 
             { 3, 10m }, 
-            { 4, 20m }, 
+            { 4, 15m }, 
             { 5, 25m },
             { 6, 30m }, 
             { 7, 35m }
--- AO.KataPotter/AO.KataPotter.Implementation/Business/CalculatorStrategy/LinqStrategyCalculator.cs
+++ AO.KataPotter/AO.KataPotter.Implementation/Business/CalculatorStrategy/LinqStrategyCalculator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Linq;
+using AO.KataPotter.Implementation.Entities;
 using AO.KataPotter.Interfaces.Business;
 using AO.KataPotter.Interfaces.Entities;
 
@@ -8,7 +9,12 @@ namespace AO.KataPotter.Implementation.Business.CalculatorStrategy
     {
         public override IShoppingCartPrice CalculateCartPrice(IShoppingCart shoppingCart)
         {
-            throw new NotImplementedException();
+            var bookGroups = shoppingCart.BookItems.GroupBy(p => p.Book).Select(p => p.Count()).ToList();
+
+            var booksForDiscount = bookGroups.Count * (1 - (DistinctDiscounts.Keys.Contains(bookGroups.Count) ? DistinctDiscounts[bookGroups.Count] : 0) / 100);
+            var totalPrice = DEFAULT_PRICE * (booksForDiscount + (shoppingCart.BookItems.Sum(_ => _.Quantity) - bookGroups.Count));
+
+            return new ShoppingCartPrice(totalPrice, 0);
         }
     }
 }
