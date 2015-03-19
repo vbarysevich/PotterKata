c91602c2685462ae901f132724d83bd103dc344c
vbarysevich <v.barysevich@godeltech.com>
2015-03-19 16:24:41 +0300

    Refactoring calculation method.

--- AO.KataPotter/AO.KataPotter.Implementation/Business/CalculatorStrategy/LinqStrategyCalculator.cs
+++ AO.KataPotter/AO.KataPotter.Implementation/Business/CalculatorStrategy/LinqStrategyCalculator.cs
@@ -9,10 +9,10 @@ namespace AO.KataPotter.Implementation.Business.CalculatorStrategy
     {
         public override IShoppingCartPrice CalculateCartPrice(IShoppingCart shoppingCart)
         {
-            var bookGroups = shoppingCart.BookItems.GroupBy(p => p.Book).Select(p => p.Count()).ToList();
+            var seriesCount = shoppingCart.BookItems.GroupBy(p => p.Book).Count();
 
-            var booksForDiscount = bookGroups.Count * (1 - (DistinctDiscounts.Keys.Contains(bookGroups.Count) ? DistinctDiscounts[bookGroups.Count] : 0) / 100);
-            var totalPrice = DEFAULT_PRICE * (booksForDiscount + (shoppingCart.BookItems.Sum(_ => _.Quantity) - bookGroups.Count));
+            var discountRatio = seriesCount * (1 - (DistinctDiscounts.Keys.Contains(seriesCount) ? DistinctDiscounts[seriesCount] : 0) / 100);
+            var totalPrice = DEFAULT_PRICE * (discountRatio + (shoppingCart.BookItems.Sum(_ => _.Quantity) - seriesCount));
 
             return new ShoppingCartPrice(totalPrice, 0);
         }
