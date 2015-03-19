80d358f46cc0c84335d5a01e102eac8d390654f4
vbarysevich <v.barysevich@godeltech.com>
2015-03-19 12:57:15 +0300

    Add 2 projects to the solution

---
+++ AO.KataPotter/AO.KataPotter.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AO.KataPotter.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Godel Technologies Europe")]
+[assembly: AssemblyProduct("AO.KataPotter.Interfaces")]
+[assembly: AssemblyCopyright("Copyright © Godel 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6a683fde-b8fd-49e6-9ef5-100507829796")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
